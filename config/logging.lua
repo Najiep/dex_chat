2cb8080f69a15cabcf7aa92db59b3203555251f9
Najiep <nali@mcm.edu.ph>
2026-08-04 03:37:48 +0800

    feat: add queued logging configuration

--- config/logging.lua
+++ config/logging.lua
@@ -1,9 +1,23 @@
 Config.Logging = {
     Console = true,
+    IncludeIdentifiers = true,
+
     Discord = {
         Enabled = false,
         Webhook = '',
         Username = 'Dex Chat',
-        AvatarUrl = ''
+        AvatarUrl = '',
+        MinimumIntervalMs = 750,
+        Categories = {
+            chat = true,
+            ooc = true,
+            roleplay = false,
+            advertisement = true,
+            staff = true,
+            organization = true,
+            private = true,
+            moderation = true,
+            system = false
+        }
     }
 }
